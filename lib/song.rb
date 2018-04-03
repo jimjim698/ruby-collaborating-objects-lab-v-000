5efe90cf5844c047d6a5967240c9d19380dca0a8
James Lotruglio <jimlotruglio@yahoo.com>
2018-04-03 18:31:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,9 +8,9 @@ end
 def self.new_by_filename(filename)
   split_name = filename.split(" - ")
   song = self.new(split_name[1])
-  song.artist = artist
   artist = Artist.find_or_create_by_name(split_name[0])
   artist.add_song(song)
+  song.artist = artist
   song
 end
 
