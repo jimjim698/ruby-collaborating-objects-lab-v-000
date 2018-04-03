cd5c4aa3b31554f0586f992bc6525b13357ca09a
James Lotruglio <jimlotruglio@yahoo.com>
2018-04-03 21:32:28 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,14 +1,22 @@
 class MP3Importer
-attr_accessor :path, :files
+  attr_accessor :path
 
-def initialize(file)
-  @path = file
-@files = Dir.entries(path).grep(/.*\.mp3/)
-end
+  def initialize(filepath)
+    @path = filepath
+  end
+
+  def files
+    @files = Dir.entries(@path)
+    @files.delete_if {|file| file == "." || file == ".."}
+  end
 
 
-def import
-@files.each {|file| Song.new_by_filename(file)}
-end
+  def import
+    self.files.each do |file|
+      song = Song.new_by_filename(file)
+      Artist.all<< song.artist unless
+      Artist.all.include?(Song.artist)
+    end
+  end
 
 end
