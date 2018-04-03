f0bc770ddbf104aee83bedfa160d9efd1fd41a0d
James Lotruglio <jimlotruglio@yahoo.com>
2018-04-03 19:11:49 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -3,12 +3,9 @@ attr_accessor :path, :files
 
 def initialize(file)
   @path = file
-  @files = Dir.entries(@path)
+@files = Dir.entries(path).grep(/.*\.mp3/)
 end
 
-def files
-  @files.delete_if {|file|file == "."|| file == ".."}
-end
 
 def import
   self.files.each do |file|
